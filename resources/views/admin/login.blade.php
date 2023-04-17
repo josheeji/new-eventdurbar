ef64b14a7e6f2560d0c49a6923b108f33af6bb99
roman_awale <roman@soanitech.com>
2023-04-17 11:59:57 +0545

    User Registration is done

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -24,14 +24,14 @@
 
                             <form method="POST" action="{{ route('login.custom') }}">
                                 @csrf
-                                <div class="col-12">
+                                <div class="col-12 mb-2">
                                     <label for="email" class="form-label">Username</label>
                                     <div class="input-group has-validation">
                                         <span class="input-group-text" id="inputGroupPrepend">@</span>
                                         <input type="text" name="email" class="form-control" id="email"
                                             required="">
                                         <div class="invalid-feedback">Please enter your username.</div>
-                                        
+
                                         @if ($errors->has('email'))
                                             <span class="text-danger">{{ $errors->first('email') }}</span>
                                         @endif
@@ -39,7 +39,7 @@
 
                                     <br>
 
-                                    <div class="col-12">
+                                    <div class="col-12 mb-3">
                                         <label for="password" class="form-label">Password</label>
                                         <input type="password" name="password" class="form-control" id="yourPassword"
                                             required="">
@@ -56,12 +56,16 @@
                                         <label class="form-check-label" for="rememberMe">Remember me</label>
                                     </div>
                                 </div> --}}
-                                    <br>
-                                    <div class="col-12">
+                                   
+                                    <div class="col-12 mb-3"  >
                                         <button class="btn btn-primary w-100" type="submit">Login</button>
-                                    </div><br>
+                                    </div>
                                     <div class="col-12">
-                                        <button class="btn btn-primary w-100" type="submit">Register</button>
+
+                                        <a title="Edit" href="/admin/register"
+                                            class="btn btn-primary w-100">Register</a>
+
+
                                     </div>
                                     {{-- <div class="col-12">
                                     <p class="small mb-0">Don't have account? <a href="pages-register.html">Create
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -13,8 +13,17 @@
                             <span class="d-none d-lg-block">Durbar Event</span>
                         </a>
                     </div><!-- End Logo -->
-
+                    {{-- list the errors --}}
                     <div class="card mb-3">
+                        @if ($errors->any())
+                        <div class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <div>{{ $error }}</div>
+                            @endforeach
+                        </div>
+                    @endif
+                    <hr>
+                    {{-- list end --}}
 
                         <div class="card-body">
 
@@ -32,18 +41,18 @@
                                     <div class="invalid-feedback">Please, enter your name!</div>
                                 </div>
 
-                                <div class="col-12">
+                                {{-- <div class="col-12">
                                     <label for="yourEmail" class="form-label">Your Email</label>
                                     <input type="email" name="email" class="form-control" id="yourEmail"
                                         required="">
                                     <div class="invalid-feedback">Please enter a valid Email adddress!</div>
-                                </div>
+                                </div> --}}
 
                                 <div class="col-12">
-                                    <label for="yourUsername" class="form-label">Username</label>
+                                    <label for="yourEmail" class="form-label">Your Email</label>
                                     <div class="input-group has-validation">
                                         <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                        <input type="text" name="username" class="form-control" id="yourUsername"
+                                        <input type="text" name="email" class="form-control" id="yourUsername"
                                             required="">
                                         <div class="invalid-feedback">Please choose a username.</div>
                                     </div>
@@ -54,7 +63,7 @@
                                     <input type="password" name="password" class="form-control" id="yourPassword"
                                         required="">
                                     <div class="invalid-feedback">Please enter your password!</div>
-                                </div>
+                                </div><br>
 
                                 {{-- <div class="col-12">
                                     <div class="form-check">
@@ -68,10 +77,10 @@
                                 <div class="col-12">
                                     <button class="btn btn-primary w-100" type="submit">Create Account</button>
                                 </div>
-                                {{-- <div class="col-12">
-                                    <p class="small mb-0">Already have an account? <a href="pages-login.html">Log in</a>
+                                <div class="col-12">
+                                    <p class="small mb-0">Already have an account? <a href="login">Log in</a>
                                     </p>
-                                </div> --}}
+                                </div>
                             </form>
 
                         </div>
