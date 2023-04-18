05a49e0cfa3bf6b69429ad204419be4a871e8f25
roman_awale <roman@soanitech.com>
2023-04-18 10:48:54 +0545

    EventSeeder is added in the database

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'name' => 'Event A',
+                'image' => 'https://example.com/images/event-a.jpg',
+                'short_description' => 'Short description for Event A',
+            ],
+            [
+                'name' => 'Event B',
+                'image' => 'https://example.com/images/event-b.jpg',
+                'short_description' => 'Short description for Event B',
+            ],
+            [
+                'name' => 'Event C',
+                'image' => 'https://example.com/images/event-c.jpg',
+                'short_description' => 'Short description for Event C',
+            ],
+            // Add more data as needed
+        ];
+        foreach($data as $item){
+            Event::firstOrCreate($item);
+        }
+
+       
+    }
+}
+
---
+++ database/seeders/ParticipationTypeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ParticipantType;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ParticipationTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
